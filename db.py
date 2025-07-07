a8aa78bec4e020eb14ca8535fd49b94f38c152e7
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-07-07 10:06:11 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,8 +3,8 @@ from pymongo import MongoClient
 # 🧠 MongoDB Setup
 MONGO_URL = "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # or your Atlas URI
 
-client = MongoClient(MONGO_URL)
-db = client["RenameBot"]
+client = MongoClient(MONGO_URL)  
+db = client["rename_bot"]
 col = db["users"]
 
 def get_settings(user_id):
@@ -14,7 +14,8 @@ def get_settings(user_id):
             "_id": user_id,
             "screenshot": False,
             "count": 3,
-            "prefix": True,
+            "prefix_enabled": True,
+            "prefix_text": "@sunriseseditsoffical6 -",
             "rename_type": "doc",
             "caption_style": "bold",
             "thumb": None
@@ -27,11 +28,11 @@ def update_settings(user_id, key, value):
     col.update_one({"_id": user_id}, {"$set": {key: value}}, upsert=True)
 
 def set_thumbnail(user_id, file_id):
-    update_settings(user_id, "thumbnail", file_id)
+    update_settings(user_id, "thumb", file_id)
 
 def get_thumbnail(user_id):
-    user = get_settings(user_id)
-    return user.get("thumbnail")
+    data = get_settings(user_id)
+    return data.get("thumb")
 
 def clear_thumbnail(user_id):
-    update_settings(user_id, "thumbnail", None)
+    update_settings(user_id, "thumb", None)
