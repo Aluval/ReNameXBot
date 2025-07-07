883a5be3cefee7db9bec457c4651ed0df7594322
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-07-07 10:20:51 +0530

    Update db.py

--- db.py
+++ db.py
@@ -3,36 +3,36 @@ from pymongo import MongoClient
 # 🧠 MongoDB Setup
 MONGO_URL = "mongodb+srv://HARSHA24:HARSHA24@cluster0.sxaj8up.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"  # or your Atlas URI
 
-client = MongoClient(MONGO_URL)  
-db = client["rename_bot"]
-col = db["users"]
+client = MongoClient(MONGO_URL)
+db = client.rename_bot
+collection = db.users
 
-def get_settings(user_id):
-    data = col.find_one({"_id": user_id})
-    if not data:
+def get_settings(user_id: int) -> dict:
+    user = collection.find_one({"id": user_id})
+    if not user:
         default = {
-            "_id": user_id,
+            "id": user_id,
             "screenshot": False,
             "count": 3,
             "prefix_enabled": True,
             "prefix_text": "@sunriseseditsoffical6 -",
-            "rename_type": "doc",
-            "caption_style": "bold",
-            "thumb": None
+            "rename_type": "doc",  # "video" or "doc"
+            "caption_style": "bold",  # bold, italic, code, mono, plain
+            "thumbnail": None
         }
-        col.insert_one(default)
+        collection.insert_one(default)
         return default
-    return data
+    return user
 
-def update_settings(user_id, key, value):
-    col.update_one({"_id": user_id}, {"$set": {key: value}}, upsert=True)
+def update_settings(user_id: int, key: str, value):
+    collection.update_one({"id": user_id}, {"$set": {key: value}})
 
-def set_thumbnail(user_id, file_id):
-    update_settings(user_id, "thumb", file_id)
+def set_thumbnail(user_id: int, file_id: str):
+    update_settings(user_id, "thumbnail", file_id)
 
-def get_thumbnail(user_id):
-    data = get_settings(user_id)
-    return data.get("thumb")
+def get_thumbnail(user_id: int) -> str | None:
+    user = get_settings(user_id)
+    return user.get("thumbnail")
 
-def clear_thumbnail(user_id):
-    update_settings(user_id, "thumb", None)
+def clear_thumbnail(user_id: int):
+    update_settings(user_id, "thumbnail", None)
