6ec2dd64fc77d1a24a8217346329957c4209d752
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-07-07 09:54:44 +0530

    Update db.py

--- db.py
+++ db.py
@@ -8,8 +8,8 @@ db = client["RenameBot"]
 col = db["users"]
 
 def get_settings(user_id):
-    user = col.find_one({"_id": user_id})
-    if not user:
+    data = col.find_one({"_id": user_id})
+    if not data:
         default = {
             "_id": user_id,
             "screenshot": False,
@@ -17,12 +17,11 @@ def get_settings(user_id):
             "prefix": True,
             "rename_type": "doc",
             "caption_style": "bold",
-            "thumbnail": None,
-            "custom_prefix": "@sunriseseditsoffical6 -"
+            "thumb": None
         }
         col.insert_one(default)
         return default
-    return user
+    return data
 
 def update_settings(user_id, key, value):
     col.update_one({"_id": user_id}, {"$set": {key: value}}, upsert=True)
