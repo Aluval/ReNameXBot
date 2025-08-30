c7007570eabf42d5a33866a793011fbd72051761
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-08-31 00:36:17 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -617,7 +617,7 @@ async def rename_link(client, message: Message):
                     async for chunk in resp.content.iter_chunked(1024 * 1024):
                         f.write(chunk)
                         downloaded += len(chunk)
-                        progress_bar_link(downloaded, total_size, task)
+                        await progress_bar_link(downloaded, total_size, task)
 
         await task["message"].edit("✅ Download complete.")
 
