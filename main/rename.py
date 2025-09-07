4462e9e54676df62db4a369625a91a7b809b4796
ALUVALA EDIGA HARSHA VARDHAN GOUD <95118956+Aluval@users.noreply.github.com>
2025-09-07 10:45:54 +0530

    Update rename.py

--- main/rename.py
+++ main/rename.py
@@ -20,7 +20,7 @@ from main.db import (
     add_task,
     get_user_tasks,
     get_all_user_tasks,
-    remove_task_and_file_by_id,
+    remove_task_by_file_id,
     save_file,
     get_saved_file,
     get_user_files,
